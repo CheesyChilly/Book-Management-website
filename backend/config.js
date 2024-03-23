52ccdd1090490ce6131ed93ec7082f74907249ea
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-03-24 01:12:49 +0530

    Adding MongoDB APIs
    Setting up basic adding, updating, searching and deleting operation on books from the MongoDB database.

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const MONGODB_URL =
+  "mongodb+srv://boseankit:boseankit@bookstore-mern.lttrddk.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,126 @@
 import express from "express";
-import {PORT} from "./config.js";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/',(request, response)=>{
+// Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Welcome to MERN stack tutorial");
+  return response.status(200).send("Welcome to my MERN stack project.");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+// Route for a new book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publisherYear
+    ) {
+      return response.status(400).send({
+        message: "Send all rqquired fields: title, author, publisherYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publisherYear: request.body.publisherYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
+
+// Route for Getting all books from Database
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for getting One book by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for updating a book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publisherYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publisherYear",
+      });
+    }
+
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route to delete a book
+app.delete('/books/:id', async (request, response)=>{
+  try{
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result){
+      return response.status(404).json({ message: "Book not found" });
+    }
+    return response.status(200).send({ message: "Book deleted successfully" });
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+})
+
+
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("App connect to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
