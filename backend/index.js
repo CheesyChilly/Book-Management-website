1b291256fc66b177fe1ff5f02392d858897ef07d
CheesyChilly <97239118+CheesyChilly@users.noreply.github.com>
2024-03-27 00:20:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,27 @@ app.use(express.json());
 // app.use(cors());
 
 // Allow CORS requests from specific origin
-app.use(
-  cors({
-    origin: "https://managingbooks.netlify.app", // Replace with your allowed origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods (optional)
-    credentials: true, // Allow sending cookies (optional)
-    allowedHeaders: ["Content-Type"], // Allowed request headers (optional)
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://managingbooks.netlify.app",'http://localhost:5173'], // Replace with your allowed origin
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods (optional)
+//     credentials: true, // Allow sending cookies (optional)
+//     allowedHeaders: ["Content-Type"], // Allowed request headers (optional)
+//   })
+// );
+
+const allowedOrigins = ["https://managingbooks.netlify.app",'http://localhost:5173'];
+
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // sending a basic response to the webpage
 app.get("/", (request, response) => {
